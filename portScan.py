4c77a3e27ff09a8b93b59def8bf1ab17e3a1a290
Fayaz Akhtar <49790444+fayaxa@users.noreply.github.com>
2019-04-20 16:18:34 +0100

    Update portScan.py

--- portScan.py
+++ portScan.py
@@ -1,44 +1,42 @@
 '''
-A simple Pyhton-3 script to scan for open ports.
+A simple script to scan for open ports.
 '''
 import sys
 import time
 import socket
 
 def scan_host(start_port, end_port, host_ip):
-	for port in range(start_port, end_port + 1):
-		try:
-			with socket.socket(socket.AF_INET, 
-						socket.SOCK_STREAM) as sock:
-            
-				code = sock.connect_ex((host_ip, port))
-				if code == 0:
-					print("[*] Port {}: Open".format(port))
-          
-		except KeyboardInterrupt:
-			print ("\n[!] Keyboard Interrupt, Exiting...")
-			sys.exit(0)
-		except Exception as e:
-			print ("\n[!] Error: {}".format(e))
-	return
+    for port in range(start_port, end_port + 1):
+        try:
+            with socket.socket(socket.AF_INET, 
+                    socket.SOCK_STREAM) as sock:
+
+                code = sock.connect_ex((host_ip, port))
+                if code == 0:
+                    print("[*] Port {}: Open".format(port))
+
+        except KeyboardInterrupt:
+            print ("\n[!] Keyboard Interrupt, Exiting...")
+            sys.exit(0)
+        except Exception as e:
+            print ("\n[!] Error: {}".format(e))
+    return
 
 def main():
-	host = input("\n[*] Enter the Host to be Scanned: ")
-	start_port = int(input("[*] Enter Start Port Number: "))
-	end_port = int(input("[*] Enter End Port Number: "))
+    host = input("\n[*] Enter the Host to be Scanned: ")
+    start_port = int(input("[*] Enter Start Port Number: "))
+    end_port = int(input("[*] Enter End Port Number: "))
 
-	host_ip = socket.gethostbyname(host)
-	print("\n[*] Host IP: {}".format(host_ip))
+    host_ip = socket.gethostbyname(host)
+    print("\n[*] Host IP: {}".format(host_ip))
 
-	print("\n[*] Scanning....")
-	start_time = time.time()
+    print("\n[*] Scanning....")
+    start_time = time.time()
 
-	scan_host(start_port, end_port, host_ip)        
+    scan_host(start_port, end_port, host_ip)        
 
-	total_time = time.time() - start_time
-	print("\n[*] Scanning Duration: {}".format(total_time))
+    total_time = time.time() - start_time
+    print("\n[*] Scanning Duration: {}".format(total_time))
 
 if __name__ == "__main__":
-	main()
-
-  
+    main()
